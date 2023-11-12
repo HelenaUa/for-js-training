9a16bdb4c069e1a9daa207f1a9ab2ace71564f09
HelenaUa <110732166+HelenaUa@users.noreply.github.com>
2023-11-12 13:59:16 +0200

    add module 1

---
+++ index.js
@@ -0,0 +1,193 @@
+// Модуль 1. Змінні та типи. Розгалуження. Цикли.
+// Модуль 1 (Урок 1)
+
+// const value = prompt('Java Script');
+  // if(value === 'js') {
+  //   alert('YES');
+  // } else {
+  //   alert('NO')
+  // }
+  // console.log(value);
+
+  // const userInput = Number(prompt('Введи число'));
+  // console.log(typeof userInput);
+  // if(!userInput) {
+  //   console.log('це нуль');
+  // } else if(userInput > 0) {
+  //   console.log('yes');
+  // } else {
+  //   console.log('no');
+  // };
+
+  // const userInput = Number(prompt('Введи число'));
+  // console.log(userInput % 2 ? 'не парне' : 'парне');
+
+  // if(!0) {
+  //   console.log('00000')
+  // } else {
+  //   console.log('0')
+  // }
+
+// Модуль 1 (Урок 2)
+
+  // const a = 90;
+  // const b = 130;
+  // if(a > 100 && b > 100) {
+  //   console.log(a > b ? a : b);
+  // }else {
+  //   console.log(b + 512);
+  // }
+
+  // let link = 'https://my-site.com/about';
+  // if(!(link[link.length - 1] === '/')) {
+  //   link += '/';
+  // };
+  // console.log(link);
+
+  // let link = 'https://my-site.com/about';
+  // if(!link.endsWith('/')) {
+  //   link += '/'
+  // };
+  // console.log(link);
+
+  // let link = 'https://my-site.com/about';
+  // if(!link.endsWith('/') && link.includes('my-site')) {
+  //     link += '/'
+  // } 
+  // console.log(link);
+
+  // let link = 'https://my-site.com/about';
+  // console.log(!link.endsWith('/') && link.includes('my-site') ? link += '/' : link);
+
+  // const daysDeadline = 5;
+  // switch(daysDeadline) {
+  //   case 0: console.log('сьогодні');
+  //   break;
+  //   case 1: console.log('завтра');
+  //   break;
+  //   case 2: console.log('після завтра');
+  //   break;
+  //   default: console.log('дата у майбутньому');
+  // };
+
+  // const min = 50;
+  // const max = 100;
+  // for(let i = min; i <= max && i >= min; i +=1) {
+  //   if(!(i % 5)) {
+  //     console.log(i);
+  //   }
+  // }
+
+
+// Модуль 2. Масиви. Функції.
+// Модуль 2 (Урок 1)
+
+  // const array = [1, 'Hello', null, true, [1, 2, 3]];
+  // for(let i = 0; i < array.length; i += 1) {
+  //   if(Array.isArray(array[i])) {
+  //     for(let j = 0; j < array[i].length; j +=1) {
+  //       console.log(array[i][j])
+  //     }
+  //     continue;
+  //   }
+  //   console.log(array[i]);
+  // }
+
+  // const array = [1, 'Hello', null, true, [1, 2, 3]];
+  // for(let i = 0; i < array.length; i += 1) {
+  //   const item = array[i];
+  //   if(Array.isArray(item)) {
+  //     for(let j = 0; j < item.length; j +=1) {
+  //       console.log(item[j])
+  //     }
+  //     continue;
+  //   }
+  //   console.log(array[i]);
+  // }
+
+  // const array = [1, 'Hello', null, true, [1, 2, 3]];
+  // for(const item of array) {
+  //   console.log(item);
+  // }
+
+  // const values = '8 11';
+  // const array = values.split(' ');
+  // const first = Number(array[0]);
+  // const last = Number(array[array.length-1]);
+  // const result = first * last;
+  // console.log(result);
+
+  // let names = 'Poli, Doli, Mango';
+  // let number = '0501112233, 0661472536, 0951475623';
+  // names = names.split(',');
+  // number = number.split(',');
+  // for(let i = 0; i < names.length, i < number.length; i += 1) {
+  //   console.log(`${names[i]} - ${number[i]}`);
+  // };
+ 
+  // const string = 'Welcome to the future';
+  // const result = string.split(' ').slice(1, -1).join(' ');
+  // console.log(result);
+
+  // const langs = ['cb', 'as', 'dfg', 'op'];
+  // for(let i = 0; i < langs.length; i +=1) {
+  //   if(langs[0] > langs[i]) {
+  //     const result = langs.splice(i,1)[0];
+  //     langs.unshift(result);
+  //   }
+  // };
+  // console.log(langs);
+
+  const numbers = [2, 5, 94, 6, 1, 57];
+  let min = numbers[0];
+  for (const number of numbers) {
+    if(number < min) {
+      min = number
+    }
+  };
+  console.log(min);
+
+// Модуль 2 (Урок 2)
+
+
+
+
+
+
+
+
+
+
+
+// Задача з технічної співбесіди
+// function isPossibleToCompose(word, letters) {
+  //   const wordMap = {}
+  //   const lettersMap = {}
+    
+  //   for (let i=0; i<word.length; i++){
+  //     const char = word[i] 
+  //     wordMap[char] = (wordMap[char] || 0) + 1  
+  //   }
+  
+  //   for (let i=0; i<letters.length; i++){
+  //     const char = letters[i]
+  //     lettersMap[char] = (lettersMap[char] || 0) + 1  
+  //   }
+    
+  //   console.log({
+  //     wordMap,
+  //     lettersMap,
+  //   })
+  
+  //   for (const char in wordMap) {
+  //     if (!lettersMap[char] || lettersMap[char] < wordMap[char]) {
+  //       return false
+  //     }
+  //   }
+  
+  //   return true
+  // }
+  
+  // console.log(isPossibleToCompose('aa', 'abc')) // false
+  // isPossibleToCompose('abc', 'dcba') // true
+  // isPossibleToCompose('aabbcc', 'bcabcaddff') // true
